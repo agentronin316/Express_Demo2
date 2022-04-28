3870522aa281f0c04856f29964aab63804074f1b
agentronin316 <agentronin316@users.noreply.github.com>
2022-04-28 17:16:23 -0500

    added endpoints

--- index.js
+++ index.js
@@ -10,8 +10,85 @@ server.listen(port, () => console.log("server listening"));
 
 const destinations = [];
 
-server.get("/marshall", (req, res) => {
-    res.send("<h1>Would you like to play a game?</h1>");
+const students = {
+    will: { name: "Will", city: "Detroit", interests: ["cars"] },
+    nikko: { name: "Nikko", city: "Detroit", interests: ["bananas"] },
+    marshall: {
+        name: "Marshall",
+        city: "Kansas City",
+        interests: ["acting"],
+    },
+    mannie: {
+        name: "Mannie",
+        city: "Georgia",
+        interests: ["soccer", "bananas"],
+    },
+};
+
+server.get("/students/name/:name", (req, res) => {
+    if (name) {
+        const student = students[name.toLowerCase()];
+        if (student) {
+            return res.send(student);
+        }
+        return res
+            .status(404)
+            .send({ error: `Student named ${name} not found` });
+    }
+});
+
+server.get("/students/city/:city", (req, res) => {
+    if (city) {
+        return res.send(
+            tempStudents.filter(
+                (stu) => stu.city.toLowerCase() === city.toLowerCase()
+            )
+        );
+    }
+});
+
+server.get("/students/interest/:interest", (req, res) => {
+    if (interest) {
+        return res.send(
+            tempStudents.filter((stu) =>
+                stu.interests.includes(interest.toLowerCase())
+            )
+        );
+    }
+});
+
+server.get("/students", (req, res) => {
+    const { name, interest, city } = req.query;
+
+    if (name) {
+        const student = students[name.toLowerCase()];
+        if (student) {
+            return res.send(student);
+        }
+        return res
+            .status(404)
+            .send({ error: `Student named ${name} not found` });
+    }
+
+    let tempStudents = Object.values(students);
+
+    if (city) {
+        tempStudents = tempStudents.filter(
+            (stu) => stu.city.toLowerCase() === city.toLowerCase()
+        );
+    }
+
+    if (interest) {
+        tempStudents = tempStudents.filter((stu) =>
+            stu.interests.includes(interest.toLowerCase())
+        );
+    }
+
+    return res.send(tempStudents);
+});
+
+server.get("/destinations", (req, res) => {
+    res.send(destinations);
 });
 
 server.get("/coffee", (req, res) => {
@@ -29,7 +106,15 @@ server.post("/destinations", (req, res) => {
         return res.status(400).send({
             error: "Destination and location are both required",
         });
-    } else {
-        console.log(req.body);
     }
+    const newDestination = {
+        destination,
+        location,
+        photo: photo && photo.length ? photo : "insert placeholder here",
+        description: description ? description : "",
+    };
+
+    destinations.push(newDestination);
+
+    res.redirect(303, "/destinations");
 });
